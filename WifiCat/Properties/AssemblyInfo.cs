9f90cc6d4cf6ecec4e490f9dfa092cb75b00a3d8
Bennet Becker <bennet@becker-dd.de>
2017-06-07 20:35:56 +0200

    code siging

--- WifiCat/Properties/AssemblyInfo.cs
+++ WifiCat/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WifiCat")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Wifi Configuration Assistent")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WifiCat")]
