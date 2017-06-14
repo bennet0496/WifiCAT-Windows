153b48c65525a04590d0438f17a1f95d75da635d
Bennet Becker <bennet@becker-dd.de>
2017-06-14 22:29:02 +0200

    restructure project

--- WifiCat/FormInstallNetwork.cs
+++ WifiCat/FormInstallNetwork.cs
@@ -1,10 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region Licence
+
+// WifiCat - WifiCat - FormInstallNetwork.cs
+// Created 14.06.2017 19:08
+// 
+// 
+// Wifi Configuration Assist Tool
+// Copyright (C) 2017 Bennet Becker <bennet@becker-dd.de>
+// 
+// This program is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU Affero General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License 
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region Using
+
+using System;
 using System.Linq;
 using System.Windows.Forms;
 using System.Xml.Linq;
 using NativeWifi;
 using WifiCat.Properties;
+
+#endregion
 
 namespace WifiCat
 {
@@ -100,7 +127,7 @@ namespace WifiCat
                 tb_password.Enabled = false;
                 cb_wifiinterface.Enabled = false;
                 b_install_network.Enabled = false;
-                ((WlanClient.WlanInterface)cb_wifiinterface.SelectedItem).DeleteProfile(Properties.Resources.SSID_TO_DELETE);
+                ((WlanClient.WlanInterface) cb_wifiinterface.SelectedItem).DeleteProfile(Resources.SSID_TO_DELETE);
             }
             else
             {
--- WifiCat/Program.cs
+++ WifiCat/Program.cs
@@ -1,5 +1,33 @@
-﻿using System;
+﻿#region Licence
+
+// WifiCat - WifiCat - Program.cs
+// Created 14.06.2017 19:08
+// 
+// 
+// Wifi Configuration Assist Tool
+// Copyright (C) 2017 Bennet Becker <bennet@becker-dd.de>
+// 
+// This program is free software: you can redistribute it and/or modify 
+// it under the terms of the GNU Affero General Public License as published by 
+// the Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License 
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region Using
+
+using System;
 using System.Windows.Forms;
+
+#endregion
 
 namespace WifiCat
 {
